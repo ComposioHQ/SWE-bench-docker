924beec1efbf7d88028f315511184c6741d42e9b
Shubhra Srivastava <shubhras01@gmail.com>
2024-06-24 19:38:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+Setup configuration for swe-bench-docker
+"""
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="swe_bench_docker",
+    version="0.0.1",
+    author="aorwall",
+    author_email="",
+    description="Dockerization of run_evaluation.py",
+    long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/aorwall/SWE-bench-docker",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9,<4",
+    packages=find_packages(include=["swe_bench_docker/*"]),
+    install_requires=[],
+    include_package_data=True,
+)
